9fc18c88fd102d0c1d86a65c061e832bc450c015
Numa <numanp_92@hotmail.com>
2018-09-19 11:17:38 -0300

    template and first view of the app, added

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const nunjucks = require('nunjucks')
 
-app.use(function(req, res, next){
-	res.send('<h1>main page</h1>');
-	next();
-});
+
+// app.use(function(req, res, next){
+// 	res.send('<h1>main page</h1>');
+// 	next();
+// });
 
 //morgan(':method :url :status :res[content-length] - :response-time ms')
 
-app.use('/special', function(req, res, next){
+app.use('/', function(req, res, next){
 	console.log('llegaste a un lugar especial');
-	res.send('<h1>llegaste a un lugar especial</h1>')
+	const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 	next();
 });
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure('views', { noCache: true }) // apunta a nunjucks al directorio correcto para los templates
+
 
 console.log(chalk.blue('Hello World'))
 
